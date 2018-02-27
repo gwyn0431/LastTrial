2997eb8fb52f00a3a235e2d04537b9cd71b2d35e
gwyn0431 <gwyn0431@gmail.com>
2018-02-28 00:57:52 +0300

    Now getting NextWaypointIndex from C++

--- Source/LastTrial/Private/ChooseNextWaypoint.cpp
+++ Source/LastTrial/Private/ChooseNextWaypoint.cpp
@@ -1,10 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "ChooseNextWaypoint.h"
+#include "../Public/ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("C++ god is here!"))
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index)
+
 	return EBTNodeResult::Succeeded;
 }
 
